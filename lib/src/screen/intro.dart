4975341295748f64b69b4c0c011b06a69a735c8d
jongmun <jongmun.choi@illuminarean.com>
2025-04-15 18:59:11 +0900

    [Flutter] 예제 소스 따라해보기
    페이지 전환
    - Navigator를 이용한 전환까지

---
+++ lib/src/screen/intro.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'auth/register.dart';
+
+class Intro extends StatelessWidget {
+  const Intro({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text('이 책은 플러터로 \nSNS 중고 거래 애플리케이션을 만듭니다',
+                  textAlign: TextAlign.center),
+                  const SizedBox(height : 20),
+                  ElevatedButton(onPressed: (){
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Register()),
+                    );
+
+                  },
+                      child: const Text('사용하러 가기'))
+                ] // children
+            )
+        )
+    );
+  }
+
+}
